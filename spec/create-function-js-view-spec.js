cdd67e7952bcbe5d6892b725554d6018b82d89b4
Unknown <d3vunivers@gmail.com>
2018-03-02 16:27:54 +0100

    yolo

---
+++ spec/create-function-js-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CreateFunctionJsView from '../lib/create-function-js-view';
+
+describe('CreateFunctionJsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
